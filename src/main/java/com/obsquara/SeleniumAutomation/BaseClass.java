d05c7d332ca6e07ad9ef01f17c4e311b8a651bc6
VyshnaKL <118542176+VyshnaKL@users.noreply.github.com>
2023-02-12 21:55:07 +0530

    Update BaseClass.java

--- src/main/java/com/obsquara/SeleniumAutomation/BaseClass.java
+++ src/main/java/com/obsquara/SeleniumAutomation/BaseClass.java
@@ -21,7 +21,7 @@ public class BaseClass {
 		 System.out.println(" title are eqaul");
 	 }
 	 else {
-		 System.out.println(" Titles not equal");
+		 System.out.println(" Title not equal");
 	 }
 	}
  public void getURLcommand() {
