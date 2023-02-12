2e6f688bc384891143666c80ac6fdf592dabfff0
VyshnaKL <vyshnakl1995@gmail.com>
2023-02-12 21:28:36 +0530

    initial commit

---
+++ src/main/java/com/obsquara/SeleniumAutomation/BaseClass.java
@@ -0,0 +1,39 @@
+package com.obsquara.SeleniumAutomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class BaseClass {
+	public WebDriver driver;
+	
+
+	public void intialisebrowser() {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\Manu\\git\\SeleniumAutomation\\SeleniumAutomation\\src\\main\\java\\Resource\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+	}
+ public void getTitlecommands() {
+	 String title = driver.getTitle();
+	 String actualTitle ="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+	 System.out.println( title );
+	 if(title.equals(actualTitle)){
+		 System.out.println(" title are eqaul");
+	 }
+	 else {
+		 System.out.println(" title not equal");
+	 }
+	}
+ public void getURLcommand() {
+	 String url= driver.getCurrentUrl();
+ }
+	public static void main(String[] args) {
+
+		BaseClass base= new BaseClass();
+		base.intialisebrowser();
+        base. getTitlecommands();
+        base.getURLcommand();
+        
+	}
+
+}
